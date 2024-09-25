de2d6adb64bdebeacd2a625b6177df46f288ea87
Robert Davidyan <robert.davidyan@Roberts-MacBook-Pro.local>
2024-09-25 18:17:22 +0400

    code refactoring

--- 16-09-24/my-app/src/App.jsx
+++ 16-09-24/my-app/src/App.jsx
@@ -4,45 +4,41 @@ import img1 from "./assets/mountains.jpg";
 import img2 from "./assets/nature.jpg";
 import img3 from "./assets/snow.jpg";
 
+const images = [
+  {
+    id: 1,
+    src: img1,
+    text: "Mountains",
+  },
+  {
+    id: 2,
+    src: img2,
+    text: "Nature",
+  },
+  {
+    id: 3,
+    src: img3,
+    text: "Snow",
+  },
+];
+
 class App extends Component {
   constructor() {
     super();
     this.state = { currentPhotoIndex: 0 };
   }
 
-  images = [
-    {
-      id: 1,
-      src: img1,
-      text: "Mountains",
-    },
-    {
-      id: 2,
-      src: img2,
-      text: "Nature",
-    },
-    {
-      id: 3,
-      src: img3,
-      text: "Snow",
-    },
-  ];
-
   toggleLeft = () => {
     this.setState((prevState) => ({
       currentPhotoIndex:
-        (prevState.currentPhotoIndex - 1 + this.images.length) %
-        this.images.length,
+        (prevState.currentPhotoIndex - 1 + images.length) % images.length,
     }));
   };
 
   toggleRight = () => {
-    console.log(this.state.currentPhotoIndex,'before')
     this.setState((prevState) => ({
-      currentPhotoIndex: (prevState.currentPhotoIndex + 1) % this.images.length,
+      currentPhotoIndex: (prevState.currentPhotoIndex + 1) % images.length,
     }));
-    console.log(this.state.currentPhotoIndex,'after')
-
   };
 
   handleDotClick = (index) => {
@@ -50,12 +46,12 @@ class App extends Component {
   };
 
   render() {
-    const currentImage = this.images[this.state.currentPhotoIndex];
+    const currentImage = images[this.state.currentPhotoIndex];
 
     return (
       <div>
         <SlideShow
-          count={this.images.length}
+          count={images.length}
           numImg={currentImage.id}
           srcImg={currentImage.src}
           textImg={currentImage.text}
